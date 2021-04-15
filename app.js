abbaeb43559491da108bfd865f06159adfa898ba
Michael Carr <xyeres@me.com>
2021-04-15 14:49:50 -0700

    Add routes middleware

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express');
 const morgan = require('morgan');
 const sequelize = require('./models').sequelize;
 
+// routes
+const routes = require('./routes');
+
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
@@ -20,6 +23,8 @@ app.get('/', (req, res) => {
     message: 'Welcome to the REST API project!',
   });
 });
+
+app.use('/api', routes);
 
 // send 404 if no other route matched
 app.use((req, res) => {
