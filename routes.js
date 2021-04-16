d8ebdb101098d96b9c669ad9dc440aeb0de684bb
Michael Carr <xyeres@me.com>
2021-04-15 20:05:54 -0700

    add new post, put, delete routes

--- routes.js
+++ routes.js
@@ -5,30 +5,26 @@ const express = require('express');
 
 // Middleware
 const router = express.Router();
-const { asyncHandler } = require = ('./middleware/async-handler');
+const { asyncHandler } = require('./middleware/async-handler');
 const { authenticateUser } = require('./middleware/auth-user');
 
 // Models
-const { User } = require('./models/user');
-const { Course } = require('./models/course');
+const User = require('./models').User;
+const Course = require('./models').Course;
 
 
 /*
     USER ROUTES
 */
-router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+router.get("/users", authenticateUser, asyncHandler(async (req, res) => {
     const user = req.currentUser;
-
-    res.status(200).json({ user });
+    res.status(200).json(user);
 }));
 
 
 router.post('/users', asyncHandler(async (req, res) => {
-    // TODO: Create new user
-
-    // TODO: Set Location header to "/"
-
-    res.status(201).end();
+    await User.create(req.body);
+    res.status(201).location('/').end();
 }));
 
 /*
@@ -44,7 +40,7 @@ router.get('/courses', asyncHandler(async (req, res) => {
         ],
     });
 
-    res.status(200).json({ courses });
+    res.status(200).json(courses);
 }));
 
 router.get('/courses/:id', asyncHandler(async (req, res) => {
@@ -59,6 +55,41 @@ router.get('/courses/:id', asyncHandler(async (req, res) => {
             },
         ],
     });
-    res.status(200).json({ course });
+    res.status(200).json(course);
 }));
 
+router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
+    const course = req.body;
+    // create new course
+    const newCourse = await Course.create({
+        title: course.title,
+        description: course.description,
+        userId: req.currentUser.id, // set userid to current user ID!
+    });
+
+    
+    res.status(201).location('/api/courses/' + newCourse.id).end();
+}));
+
+router.put('/courses/:id', asyncHandler(async (req, res) => {
+
+    const course = await Course.findByPk(req.params.id);
+    if (course) {
+        course.update(req.body);
+        res.status(204).end();
+    } else {
+        res.status(404).json({ message: "Course not found" });
+    }
+}));
+
+router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id);
+    if (course) {
+        await course.destroy();
+        res.status(204).end();
+    } else {
+        res.status(404).json({ message: "Course not found" });
+    }
+}));
+
+module.exports = router;
