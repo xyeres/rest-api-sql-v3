cbfeed8d583184a8697dae3a1434632bf3728aff
Michael Carr <xyeres@me.com>
2021-04-15 20:05:39 -0700

    update for new models

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -9,21 +9,23 @@ exports.authenticateUser = async (req, res, next) => {
 
     const credentials = auth(req);
 
+
     if (credentials) {
-        const user = await User.findOne({ where: { username: credentials.name } });
+        const user = await User.findOne({ where: { emailAddress: credentials.name } });
+        console.log("CONFIRMED PASSWORD: " + user.password)
         if (user) {
             const authenticated = bcrypt
-                .compareSync(credentials.pass, user.confirmedPassword);
+                .compareSync(credentials.pass, user.password);
             if (authenticated) {
-                console.log(`Authentication successful for username: ${user.username}`);
+                console.log(`Authentication successful for username: ${user.emailAddress}`);
 
                 // Store the user on the Request object.
                 req.currentUser = user;
             } else {
-                message = `Authentication failure for username: ${user.username}`;
+                message = `Authentication failure for username: ${user.emailAddress}`;
             }
         } else {
-            message = `User not found for username: ${credentials.name}`;
+            message = `User not found for username: ${credentials.firstName}`;
         }
     } else {
         message = 'Auth header not found';
